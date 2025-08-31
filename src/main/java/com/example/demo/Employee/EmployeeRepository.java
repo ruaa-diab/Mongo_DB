4c0379cc5371e16cd7c60bfa6d27ab5bf324ef6c
Ruaa Diab <Ruaa.Diab@exalt.ps>
2025-08-31 09:32:40 +0300

    mm

--- src/main/java/com/example/demo/Employee/EmployeeRepository.java
+++ src/main/java/com/example/demo/Employee/EmployeeRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
+
 public interface EmployeeRepository extends MongoRepository<Employee,String> {
 
 Employee findByEmailId (String emailID);
